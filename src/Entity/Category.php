01a72864ad0edce3aefae44fa9a50172c95417cb
laurentbedu <58790546+laurentbedu@users.noreply.github.com>
2021-09-21 08:18:31 +0200

    v1

---
+++ src/Entity/Category.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $icon_path;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image_path;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getIconPath(): ?string
+    {
+        return $this->icon_path;
+    }
+
+    public function setIconPath(string $icon_path): self
+    {
+        $this->icon_path = $icon_path;
+
+        return $this;
+    }
+
+    public function getImagePath(): ?string
+    {
+        return $this->image_path;
+    }
+
+    public function setImagePath(string $image_path): self
+    {
+        $this->image_path = $image_path;
+
+        return $this;
+    }
+}
